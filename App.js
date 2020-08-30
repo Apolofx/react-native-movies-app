f52fd59250ff2e311223e7c93e4bea3fb0cd9fb4
nacho <nacho.conso@gmail.com>
2020-08-30 19:47:11 -0300

    added fetchData function

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import fetchMovie from "./src/helpers/fetchAPI";
 export default function App() {
+  const [url, setURL] = useState();
+  const URL = "http://www.omdbapi.com/?apikey=5eec5adc&s=la vieja guardia";
+  const [movies, setMovies] = useState();
+  const fetchData = async () => {
+    try {
+      let response = await fetchMovie(URL);
+      setMovies(response);
+      console.log(movies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 export default function searchMovie(url) {
-  return axios(url);
+  return axios(url).then((res) => res.data.Search);
 }
 
 // http://www.omdbapi.com/?apikey=5eec5adc&s=ninja
