55b2c00d233a68a7ffd90aaf19410528312dba96
nacho <nacho.conso@gmail.com>
2020-09-01 10:55:52 -0300

    added Activity indicator

--- App.js
+++ App.js
@@ -1,35 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import fetchMovie from "./src/helpers/fetchAPI";
 import MovieList from "./src/components/movieList";
+
 export default function App() {
   const [url, setURL] = useState();
-  const URL = "http://www.omdbapi.com/?apikey=5eec5adc&s=la vieja guardia";
   const [movies, setMovies] = useState();
-  const fetchData = async () => {
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  let URL = `http://www.omdbapi.com/?apikey=5eec5adc&s=${query}`;
+
+  const fetchData = async (url) => {
     try {
-      let response = await fetchMovie(URL);
+      let response = await fetchMovie(url, 50);
       setMovies(response);
-      console.log(movies);
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchData(URL);
+  }, [query]);
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Text Input</Text>
-      <MovieList />
-    </View>
+      <Text>MOVIES</Text>
+      <TextInput
+        placeholder="Search"
+        onChangeText={(text) => setQuery(text)}
+        value={query}
+      />
+      <ActivityIndicator animating={loading} />
+      <MovieList data={movies} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
