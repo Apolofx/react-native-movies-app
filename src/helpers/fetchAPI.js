7d1df2a0d9ab7d40855c7748b7120d20a95325c8
nacho <nacho.conso@gmail.com>
2020-09-01 10:56:42 -0300

    added max_results arg to fetchAPI

--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -1,6 +1,11 @@
 const axios = require("axios");
-export default function searchMovie(url) {
-  return axios(url).then((res) => res.data.Search);
+export default async function searchMovie(url, max_results = 10) {
+  const res = await axios(url);
+  let movieList = res.data.Response === "False" ? [] : res.data.Search;
+  if (movieList.length > max_results) {
+    movieList = movieList.slice(0, max_results);
+  }
+  return movieList;
 }
 
 // http://www.omdbapi.com/?apikey=5eec5adc&s=ninja
